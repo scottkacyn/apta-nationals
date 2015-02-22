b04d8d5ecfc91cdaaec416633d75bb690b3ff484
Scott Kacyn <scott.kacyn@gmail.com>
2015-02-22 13:14:41 -0600

    Initial Commit

---
+++ APTA Nationals/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  APTA Nationals
+//
+//  Created by Scott Kacyn on 2/22/15.
+//  Copyright (c) 2015 New Coast Ventures. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
